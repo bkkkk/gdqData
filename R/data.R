0513f1a9c856a42d351e64f09a296417edcea226
Jacobo Blanco <jayb88@gmail.com>
2016-07-06 19:03:38 +0300

    Add docs for some runs data

--- R/data.R
+++ R/data.R
@@ -14,3 +14,30 @@
 #'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
 #'  (whether there are bid wars associated with the run)
 "sgdq2015_runs"
+
+#' SGDQ2014 Runs
+#'
+#' Runs shown at SGDQ2014.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"sgdq2014_runs"
+
+#' SGDQ2013 Runs
+#'
+#' Runs shown at SGDQ2013.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"sgdq2013_runs"
+
+#' SGDQ2012 Runs
+#'
+#' Runs shown at SGDQ2012.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"sgdq2012_runs"
