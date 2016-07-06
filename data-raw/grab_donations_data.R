bdc18342152fd9b48b12a94e271f20137ef8438d
Jacobo Blanco <jayb88@gmail.com>
2016-07-06 19:03:17 +0300

    Update data retrieval script

--- data-raw/grab_donations_data.R
+++ data-raw/grab_donations_data.R
@@ -32,34 +32,37 @@ devtools::use_data(sgdq2012_runs)
 sgdq2011_runs <- tidy_runs(fetch_runs("sgdq2011"))
 devtools::use_data(sgdq2011_runs)
 
-# SGDQ
-
 # agdq2016_donations <- tidy_donations(fetch_donations("agdq2016"))
+# devtools::use_data(agdq2016_donations)
+
+# AGDQ
 
 agdq2016_runs <- tidy_runs(fetch_runs("agdq2016"))
 devtools::use_data(agdq2016_runs)
 
-# agdq2015_donations <- tidy_donations(fetch_donationsfetch_donations("agdq2015"))
+# agdq2015_donations <- tidy_donations(fetch_donations("agdq2015"))
+# devtools::use_data(agdq2015_donations)
 
 agdq2015_runs <- tidy_runs(fetch_runs("agdq2015"))
 devtools::use_data(agdq2015_runs)
 
 # agdq2014_donations <- tidy_donations(fetch_donations("agdq2014"))
+# devtools::use_data(agdq2014_donations)
 
 agdq2014_runs <- tidy_runs(fetch_runs("agdq2014"))
 devtools::use_data(agdq2014_runs)
 
 # agdq2013_donations <- tidy_donations(fetch_donations("agdq2013"))
+# devtools::use_data(agdq2013_donations)
 
 agdq2013_runs <- tidy_runs(fetch_runs("agdq2013"))
 devtools::use_data(agdq2013_runs)
 
 # agdq2012_donations <- tidy_donations(fetch_donations("agdq2012"))
+# devtools::use_data(agdq2012_donations)
 
 agdq2012_runs <- tidy_runs(fetch_runs("agdq2012"))
 devtools::use_data(agdq2012_runs)
-
-# agdq2011_donations <- tidy_donations(fetch_donations("agdq2011"))
 
 # Others
 
@@ -71,6 +74,3 @@ devtools::use_data(agdq2012_runs)
 
 # spook_donations <- tidy_donations(fetch_donations("spook"))
 # devtools::use_data(spook_donations)
-
-
-
