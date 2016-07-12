168a5aacb0e067772d3c1dd32dddf678d089b41e
Jacobo Blanco <jayb88@gmail.com>
2016-07-12 21:39:05 +0300

    Add some missing documentation

--- R/data.R
+++ R/data.R
@@ -6,6 +6,7 @@
 #'  and \code{with_comment} (did the donator leave a comment with their donation)
 "sgdq2016_donations"
 
+
 #' SGDQ2016 Runs
 #'
 #' Runs shown at SGDQ2016.
@@ -24,6 +25,7 @@
 #'  and \code{with_comment} (did the donator leave a comment with their donation)
 "sgdq2015_donations"
 
+
 #' SGDQ2015 Runs
 #'
 #' Runs shown at SGDQ2015.
@@ -33,6 +35,7 @@
 #'  (whether there are bid wars associated with the run)
 "sgdq2015_runs"
 
+
 #' SGDQ2014 Donations
 #'
 #' Donations made during SGDQ2014.
@@ -40,6 +43,7 @@
 #' @format A data frame with four variables:  \code{donator}, \code{date}, \code{amount},
 #'  and \code{with_comment} (did the donator leave a comment with their donation)
 "sgdq2014_donations"
+
 
 #' SGDQ2014 Runs
 #'
@@ -50,6 +54,7 @@
 #'  (whether there are bid wars associated with the run)
 "sgdq2014_runs"
 
+
 #' SGDQ2013 Runs
 #'
 #' Runs shown at SGDQ2013.
@@ -58,6 +63,7 @@
 #'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
 #'  (whether there are bid wars associated with the run)
 "sgdq2013_runs"
+
 
 #' SGDQ2012 Runs
 #'
@@ -68,6 +74,17 @@
 #'  (whether there are bid wars associated with the run)
 "sgdq2012_runs"
 
+
+#' SGDQ2011 Runs
+#'
+#' Runs shown at SGDQ2011.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"sgdq2011_runs"
+
+
 #' JRDQ Donations
 #'
 #' Donations made during JRDQ.
@@ -75,6 +92,7 @@
 #' @format A data frame with four variables:  \code{donator}, \code{date}, \code{amount},
 #'  and \code{with_comment} (did the donator leave a comment with their donation)
 "jrdq_donations"
+
 
 #' CGDQ Donations
 #'
@@ -84,6 +102,7 @@
 #'  and \code{with_comment} (did the donator leave a comment with their donation)
 "cgdq_donations"
 
+
 #' CGDQ Donations
 #'
 #' Donations made during CGDQ.
@@ -91,3 +110,63 @@
 #' @format A data frame with four variables:  \code{donator}, \code{date}, \code{amount},
 #'  and \code{with_comment} (did the donator leave a comment with their donation)
 "spook_donations"
+
+
+#' AGDQ2011 Runs
+#'
+#' Runs shown at AGDQ2011.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"agdq2011_runs"
+
+
+#' AGDQ2012 Runs
+#'
+#' Runs shown at AGDQ2012.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"agdq2012_runs"
+
+
+#' AGDQ2013 Runs
+#'
+#' Runs shown at AGDQ2013.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"agdq2013_runs"
+
+
+#' AGDQ2014 Runs
+#'
+#' Runs shown at AGDQ2014.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"agdq2014_runs"
+
+
+#' AGDQ2015 Runs
+#'
+#' Runs shown at AGDQ2015.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"agdq2015_runs"
+
+
+#' AGDQ2016 Runs
+#'
+#' Runs shown at AGDQ2016.
+#'
+#' @format A data frame with six variables: \code{game}, \code{players}, \code{description},
+#'  \code{start_time}, \code{end_time}, and \code{has_bid_wars}
+#'  (whether there are bid wars associated with the run)
+"agdq2016_runs"
